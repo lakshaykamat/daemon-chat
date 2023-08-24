0b80f7e904e6d2050c43d24d14cd3df786d2b770
Lakshay Kamat <lakshaykamat2048@gmail.com>
2023-08-24 10:52:33 +0530

    App initialised

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, TypeScript Node.js Server!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
